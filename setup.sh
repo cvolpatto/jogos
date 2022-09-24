22c748371bc993051a83e23248c9afc5091c6376
Cesar Volpatto <cesar.volpatto@gmail.com>
2022-09-24 14:39:24 -0300

    corrigindo procfile

--- setup.sh
+++ setup.sh
@@ -5,7 +5,5 @@ email = \"cesar.volpatto@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
-headless = true\n\
-enableCORS=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
