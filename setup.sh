29d55b7b4fcac10237410f6d372b25c41a23c9eb
Cesar Volpatto <cesar.volpatto@gmail.com>
2022-09-25 12:36:49 -0300

    setup 01

--- setup.sh
+++ setup.sh
@@ -5,5 +5,7 @@ email = \"cesar.volpatto@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
+headless = true\n\
+enableCORS false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
